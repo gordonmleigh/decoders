d298f14e3895625e9936cbce0cd30cf62f7c0b57
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2022-02-13 14:37:26 +0100

    remove rollup, choose NPM instead of yarn, reconfigure

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-module.exports = {
-  preset: 'ts-jest',
+export default {
+  resolver: '<rootDir>/jest-resolver.cjs',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/", "/lib/"],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$'
+  testPathIgnorePatterns: ['/node_modules/', '/lib/', '/build/', '/dist/'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
+  transform: { '\\.[jt]sx?$': '@sucrase/jest-plugin' },
 };
