e215d711834c5f85db9fbc55fc974fb9a1640ab0
Gordon Leigh <gordonmleigh@gmail.com>
2023-05-03 20:17:34 +0200

    remove duplicate `integer` decoder

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 export * from './composite/array.js';
 export * from './composite/chain.js';
 export * from './composite/choose.js';
-export * from './composite/integer.js';
 export * from './composite/nullable.js';
 export * from './composite/object.js';
 export * from './composite/optional.js';
