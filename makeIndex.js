9c47e43d7e03036fd193011fa6cfb02d769cae70
Gordon Leigh <gordonmleigh@gmail.com>
2023-08-16 19:55:36 +0200

    remove barrel file

--- makeIndex.js
+++ makeIndex.js
@@ -1,29 +1,46 @@
-import { readdirSync, writeFileSync } from 'fs';
-import { basename, extname, join, relative } from 'path';
+import { readFile, readdir, writeFile } from 'fs/promises';
+import { basename, extname, join, relative, resolve } from 'path';
+import Prettier from 'prettier';
 
-const srcDir = process.argv[2];
-if (!srcDir) {
-  console.error(`usage: makeIndex.js <src>`);
-  process.exit(2);
-}
+const packageJson = JSON.parse(await readFile('package.json', 'utf8'));
+const srcDir = resolve('./src');
 
-const files = collectFiles(srcDir)
+const prettierConfig = await Prettier.resolveConfig('package.json');
+
+const files = (await collectFiles(srcDir))
   .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
   .map((x) => relative(srcDir, x))
   .filter((x) => x !== 'index');
 
-const index = ['// AUTO-GENERATED'].concat(
-  files.map((x) => `export * from './${x}.js';`),
+packageJson.exports = Object.fromEntries(
+  files.map((file) => [
+    `./${file}`,
+    {
+      import: `./lib/${file}.js`,
+      types: `./lib/${file}.d.ts`,
+    },
+  ]),
 );
-writeFileSync(join(srcDir, 'index.ts'), index.join('\n') + '\n');
 
-function collectFiles(dir) {
-  const files = readdirSync(dir, { withFileTypes: true });
+await writePretty('package.json', JSON.stringify(packageJson, null, 2));
+
+async function writePretty(path, data) {
+  await writeFile(
+    path,
+    Prettier.format(data, {
+      filepath: path,
+      ...prettierConfig,
+    }),
+  );
+}
+
+async function collectFiles(dir) {
+  const files = await readdir(dir, { withFileTypes: true });
   const matches = [];
 
   for (const file of files) {
     if (file.isDirectory() && file.name !== 'internal') {
-      matches.push(...collectFiles(join(dir, file.name)));
+      matches.push(...(await collectFiles(join(dir, file.name))));
     } else if (matchFile(file.name)) {
       matches.push(join(dir, basename(file.name, extname(file.name))));
     }
